238b6a6cbdf8ea6f7253799c3c0ad0c75111461e
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-02-22 21:26:07 +0530

    New Commit

---
+++ Assignment Sorting/Sorting Program/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var randomNumbers = new int[] { 5, 4, 5, 7, 6, 9, 4, 1, 1, 3, 4, 50, 56, 41 };
+
+        var sortedNumbers = MergeSort(randomNumbers.AsEnumerable());
+        Console.WriteLine("Merge Sort:");
+        PrintList(sortedNumbers);
+        Console.ReadKey();
+        // Console.Clear();
+    }
+}
+
