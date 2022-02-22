38f0913a9a75a789b6f98543242f459628923861
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-02-22 21:26:28 +0530

    New commit

---
+++ Assignment 6/LinkedList Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Wrox.ProCSharp.Collections
+{
+    class Program
+    {
+        static void Main()
+        {
+            PriorityDocumentManager pdm = new PriorityDocumentManager();
+            pdm.AddDocument(new Document("one", "Sample", 8));
+            pdm.AddDocument(new Document("two", "Sample", 3));
+            pdm.AddDocument(new Document("three", "Sample", 4));
+            pdm.AddDocument(new Document("four", "Sample", 8));
+            pdm.AddDocument(new Document("five", "Sample", 1));
+            pdm.AddDocument(new Document("six", "Sample", 9));
+            pdm.AddDocument(new Document("seven", "Sample", 1));
+            pdm.AddDocument(new Document("eight", "Sample", 1));
+
+            pdm.DisplayAllNodes();
+
+        }
+    }
+}
---
+++ Assignment 6/Stack Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Wrox.ProCSharp.Collections
+{
+    class Program
+    {
+        static void Main()
+        {
+            var alphabet = new Stack<char>();
+            alphabet.Push('A');
+            alphabet.Push('B');
+            alphabet.Push('C');
+
+            Console.Write("First iteration: ");
+            foreach (char item in alphabet)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.Write("Second iteration: ");
+            while (alphabet.Count > 0)
+            {
+                Console.Write(alphabet.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
