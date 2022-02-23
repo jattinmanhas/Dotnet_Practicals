dcecd7170b0d51d876ac1fdb8dca0d3468feb8e2
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-02-23 19:00:38 +0530

    New Commit

---
+++ Assignment Sorting/Bank Program/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+class ATM {
+    public int account_no;
+    public String account_holder_name;
+    public double avaliable_balance;
+
+    public ATM(int acc_no, String acc_name, double ava_bal) {
+        this.account_no = acc_no;
+        this.account_holder_name = acc_name;
+        this.avaliable_balance = ava_bal;
+    }
+}
+
+public delegate void WatchBalance(double n);
+
+class Program {
+
+    public static void checkBalance(double bal) {
+        if(bal < 0) {
+            Console.WriteLine("You are overdrawn");
+        } else if (bal < 10) {
+            Console.WriteLine("Your account balance is very low");
+        } else if (bal < 100) {
+            Console.WriteLine("Watch your spending carefully.");
+        } else {
+            Console.WriteLine("You have over $100 in your account");
+        }
+    }
+
+    static void Main() {
+
+        WatchBalance watch = checkBalance;
+        ATM ac1 = new ATM(55350619,"Eren",5);
+        watch(ac1.avaliable_balance);
+        Console.WriteLine("Mr. " + ac1.account_holder_name + " your current balance is " + ac1.avaliable_balance);
+
+        Console.WriteLine("\n*********************");
+
+        ATM ac2 = new ATM(86931811,"Itadori",50);
+        watch(ac2.avaliable_balance);
+        Console.WriteLine("Mr. " + ac2.account_holder_name + " your current balance is " + ac2.avaliable_balance);
+
+        Console.WriteLine("\n*********************");
+
+        ATM ac3 = new ATM(71387423,"Tanjiro",1000);
+        watch(ac3.avaliable_balance);
+        Console.WriteLine("Ms. " + ac3.account_holder_name + " your current balance is " + ac3.avaliable_balance);
+
+        Console.WriteLine("\n*********************");
+
+        ATM ac4 = new ATM(14223765,"Gojo",-500);
+        watch(ac4.avaliable_balance);
+        Console.WriteLine("Ms. " + ac4.account_holder_name + " your current balance is " + ac4.avaliable_balance);
+
+    }
+}
--- Assignment Sorting/Sorting Program/Program.cs
+++ Assignment Sorting/Sorting Program/Program.cs
@@ -1,19 +1,111 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
-class Program
+class SelectionSort
 {
-    static void Main(string[] args)
+    static public void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparision)
     {
-        var randomNumbers = new int[] { 5, 4, 5, 7, 6, 9, 4, 1, 1, 3, 4, 50, 56, 41 };
+        bool swapped = true;
+        do
+        {
+            int position, i, j;
+            swapped = false;
+            for (i = 0; i < sortArray.Count - 1; i++)
+            {
+                position = i;
+                for (j = i; j < sortArray.Count; j++)
+                {
+                    if (comparision(sortArray[position], sortArray[j]))
+                    {
+                        position = j;
+                    }
+                }
 
-        var sortedNumbers = MergeSort(randomNumbers.AsEnumerable());
-        Console.WriteLine("Merge Sort:");
-        PrintList(sortedNumbers);
-        Console.ReadKey();
-        // Console.Clear();
+                if (position != i)
+                {
+                    T temp = sortArray[i];
+                    sortArray[i] = sortArray[i + 1];
+                    sortArray[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+}
+public enum Designations
+{
+    CEO = 3,
+    CFO = 9,
+    sde = 6,
+    ba = 1,
+    pm = 5
+}
+
+class Employee
+{
+    int empid;
+    float salary;
+    public string name;
+    Designations designation;
+
+    public Employee(int EmpId, float Salary, string NAme, Designations design)
+    {
+        this.empid = EmpId;
+        this.salary = Salary;
+        this.name = NAme;
+        this.designation = design;
+
+    }
+
+    internal static bool CompareSalary(Employee e1, Employee e2)
+    {
+        return e1.salary < e2.salary;
+    }
+
+    internal static bool CompareNames(Employee e1, Employee e2)
+    {
+        if (e1.name.CompareTo(e2.name) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    internal static bool CompareDesignations(Employee e1, Employee e2)
+    {
+        return e1.designation < e2.designation;
     }
 }
 
+
+public class GenericMethods
+{
+    public static void Main(string[] args)
+    {
+        List<Employee> employeeList = new List<Employee>(30);
+        employeeList.Add(new Employee(1111, 230000, "Jean", Designations.CFO));
+
+        employeeList.Add(new Employee(2222, 11000000, "Aramin", Designations.CEO));
+
+        employeeList.Add(new Employee(3333, 451000, "Conny", Designations.sde));
+
+        employeeList.Add(new Employee(4444, 29999, "Mikasa", Designations.CEO));
+
+        employeeList.Add(new Employee(55555, 75000, "Shasha", Designations.sde));
+
+        employeeList.Add(new Employee(6666, 800000, "Annie", Designations.CFO));
+
+
+        SelectionSort.Sort<Employee>(employeeList, Employee.CompareSalary);
+
+        foreach (Employee e1 in employeeList)
+        {
+            Console.WriteLine(e1.name);
+        }
+    }
+}
